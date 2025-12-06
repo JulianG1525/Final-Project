1cfc32aa32ffd9e06d64813cf5b2aeedc6320b58
JulianG1525 <Gamet.Julian@student.greenriver.edu>
2025-12-06 02:54:13 -0800

    Added an enlarge button for images, need to improve positioning with p elements later and added favicon. Add more icons for nav bar later

---
+++ buttons.js
@@ -0,0 +1,5 @@
+function enlarge() {
+    console.log("Good job!");
+    document.getElementById("imgEnlarge").classList.toggle("imgEnlarge");
+    document.getElementById("bgEnlarge").classList.toggle("imgEnlarge");
+}
