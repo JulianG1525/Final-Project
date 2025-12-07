723307d1e23b72e151cea343637ecbd7d7f69059
JulianG1525 <Gamet.Julian@student.greenriver.edu>
2025-12-07 02:41:09 -0800

    Finished homepage and almost finished strat page start working on media queries

--- buttons.js
+++ buttons.js
@@ -2,4 +2,5 @@ function enlarge() {
     console.log("Good job!");
     document.getElementById("imgEnlarge").classList.toggle("imgEnlarge");
     document.getElementById("bgEnlarge").classList.toggle("imgEnlarge");
+    document.getElementById("hide").classList.toggle("imgEnlarge");
 } 
